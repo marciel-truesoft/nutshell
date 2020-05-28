ce561c243b2ccede54ab049b39ac04a7dfc91d3e
Marciel Sousa <marciel.truesoft@gmail.com>
2020-05-28 15:36:18 -0300

    :sparkles: Added Visual Studio Code settings.json

--- update_nutshell.sh
+++ update_nutshell.sh
@@ -1,6 +1,7 @@
 # Update config
 brew bundle dump -f --describe
 
-cp $HOME/.zshrc config/zsh/.zshrc
-cp $HOME/.hyper.js config/hyper/.hyper.js
-cp $HOME/.config/starship.toml config/starship/starship.toml
+cp "$HOME/.zshrc" config/zsh/.zshrc
+cp "$HOME/.hyper.js" config/hyper/.hyper.js
+cp "$HOME/.config/starship.toml" config/starship/starship.toml
+cp "$HOME/Library/Application Support/Code/User/settings.json" config/vscode/settings.json
