d00d110abe05066f4d5dbe3be054c4af0e213045
Marciel Sousa <marciel.truesoft@gmail.com>
2020-05-28 15:42:39 -0300

    :truck: Rename config/vscode to config/visual-studio-code to match Brewfile

--- update_nutshell.sh
+++ update_nutshell.sh
@@ -4,4 +4,4 @@ brew bundle dump -f --describe
 cp "$HOME/.zshrc" config/zsh/.zshrc
 cp "$HOME/.hyper.js" config/hyper/.hyper.js
 cp "$HOME/.config/starship.toml" config/starship/starship.toml
-cp "$HOME/Library/Application Support/Code/User/settings.json" config/vscode/settings.json
+cp "$HOME/Library/Application Support/Code/User/settings.json" config/visual-studio-code/settings.json
